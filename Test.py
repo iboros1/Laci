7acfa734b7a760877f30099ee6fb32d17a5c5d65
iboros1 <iborosppc@gmail.com>
2016-12-07 23:21:16 +0200

    first Commit

---
+++ Test.py
@@ -0,0 +1,18 @@
+import webbrowser
+import urllib.request
+
+
+log = open('testfile.html', 'w')
+
+#open = webbrowser.open_new("https://www.olx.ro/hobby-sport-turism/biciclete-fitness/biciclete/?search%5Border%5D=created_at%3Adesc")
+
+page = urllib.request.urlopen('https://www.olx.ro/hobby-sport-turism/biciclete-fitness/biciclete/?search%5Border%5D=created_at%3Adesc')
+info = urllib.request.parse_http_list(str(page))
+html = page.geturl()
+resp = page.getcode()
+print(info)
+print(html)
+
+log.write(html + '\n')
+log.write(str(resp) + '\n')
+log.close()
