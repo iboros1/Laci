ce2aaaa611633dbff503d462497d60bf0b0eecd6
Istvan Boros <istvan.boros@3pillarglobal.com>
2016-12-09 17:40:29 +0200

    write2

---
+++ Config.py
@@ -0,0 +1,3 @@
+
+page_to_open = "https://www.olx.ro/hobby-sport-turism/biciclete-fitness/biciclete/?search[order]=created_at%3Ades"
+page_nr = 10
--- Openpage.py
+++ Openpage.py
@@ -2,7 +2,7 @@ import httplib2
 from bs4 import BeautifulSoup
 import sqlite3
 import time
-
+from Config import *
 
 
 ## dd/mm/yyyy format
@@ -11,34 +11,28 @@ time = time.strftime("%d/%m/%Y")
 conn = sqlite3.connect('olx.db')
 do = conn.cursor()
 http = httplib2.Http()
-status, response = http.request('https://www.olx.ro/hobby-sport-turism/biciclete-fitness/biciclete/?search[order]=created_at%3Ades')
 
 
-soup = BeautifulSoup(response, 'html.parser')
-soup.prettify()
-attr = {'class': ['marginright5']}
-#Create table
-#do.execute('''CREATE TABLE Page
-#               (date1 text, Html1 text, date2 text, Html2 text)''')
-for link in soup.find_all('a', attr):
-    bike = link.get('href').replace(';promoted', '')
-    # Insert a row of data
-    do.execute("INSERT INTO Page (date1, Html1) VALUES ('%s','%s')" % (time, bike))
+for page_nr in range(page_nr):
+    status, response = http.request(page_to_open + '&page=' + str(page_nr))
+    soup = BeautifulSoup(response, 'html.parser')
+    soup.prettify()
+    attr = {'class': ['marginright5']}
+    #Create table
+    # do.execute('''CREATE TABLE Page
+    #              (date date, Html text unique)''')
 
-tabel1 = str(do.execute("SELECT date1 FROM Page LIMIT 1").fetchall())
-tabel1 = tabel1.replace('[(\'', '').replace('\',)]', '')
-
-print(tabel1 + time)
-
-if tabel1 >= time:
     for link in soup.find_all('a', attr):
-        bike = link.get('href').replace(';promoted', '')
+        bike = link.get('href')
+        html_link, sep, id_promoted = bike.partition('#')  # .replace(';promoted', '')
+
         # Insert a row of data
-        do.execute("UPDATE Page SET  date2=date2 WHERE 1")
+        try: do.execute("INSERT INTO Page (date, Html) VALUES ('%s','%s')" % (time, html_link)), print(html_link)
+        except:
+            print(id_promoted)
 
 
-
-# Save (commit) the changes
-conn.commit()
+    # Save (commit) the changes
+    conn.commit()
 conn.close()
-#http://stackoverflow.com/questions/7640061/trying-to-check-if-value-already-in-mysql-db
+    #http://stackoverflow.com/questions/7640061/trying-to-check-if-value-already-in-mysql-db
