726f33eaab93ddacdf217ef11015b4de35a681c0
iboros1 <iborosppc@gmail.com>
2016-12-10 15:18:48 +0200

    home

--- Openpage.py
+++ Openpage.py
@@ -5,34 +5,38 @@ import time
 from Config import *
 
 
+
 ## dd/mm/yyyy format
 time = time.strftime("%d/%m/%Y")
 
 conn = sqlite3.connect('olx.db')
 do = conn.cursor()
 http = httplib2.Http()
+results = (open('results.html', 'a'))
 
 
+#do.execute('''CREATE TABLE Page (date date, Html text unique)''')
 for page_nr in range(page_nr):
     status, response = http.request(page_to_open + '&page=' + str(page_nr))
     soup = BeautifulSoup(response, 'html.parser')
     soup.prettify()
     attr = {'class': ['marginright5']}
-    #Create table
-    # do.execute('''CREATE TABLE Page
-    #              (date date, Html text unique)''')
+
 
     for link in soup.find_all('a', attr):
         bike = link.get('href')
         html_link, sep, id_promoted = bike.partition('#')  # .replace(';promoted', '')
 
         # Insert a row of data
-        try: do.execute("INSERT INTO Page (date, Html) VALUES ('%s','%s')" % (time, html_link)), print(html_link)
+        try: do.execute("INSERT INTO Page (date, Html) VALUES ('%s','%s')" % (time, html_link)), results.write('\n' + html_link)
         except:
-            print(id_promoted)
+            print(str(page_nr) + str(html_link) + '\n')
+
 
 
     # Save (commit) the changes
     conn.commit()
+results.close()
 conn.close()
+
     #http://stackoverflow.com/questions/7640061/trying-to-check-if-value-already-in-mysql-db
--- Test.py
+++ Test.py
@@ -1,18 +1,33 @@
 import webbrowser
-import urllib.request
-import
+from bs4 import BeautifulSoup
+import httplib2
+import urllib3
 
-log = open('testfile.html', 'w')
+
+http = httplib2.Http()
+
+#log = open('testfile.html', 'w')
 
 #open = webbrowser.open_new("https://www.olx.ro/hobby-sport-turism/biciclete-fitness/biciclete/?search%5Border%5D=created_at%3Adesc")
 
-page = urllib.request.urlopen('https://www.olx.ro/hobby-sport-turism/biciclete-fitness/biciclete/?search%5Border%5D=created_at%3Adesc')
-info = urllib.request.parse_http_list(str(page))
-html = page.geturl()
-resp = page.getcode()
-print(info)
-print(html)
 
-log.write(html + '\n')
-log.write(str(resp) + '\n')
-log.close()
+url = 'www.upwork.com/ab/account-security/login'
+
+status, response = http.request('http://www.upwork.com/ab/account-security/login')
+soup = BeautifulSoup(response, 'html.parser')
+soup.prettify()
+
+
+http = urllib3.PoolManager()
+headers = urllib3.util.make_headers(basic_auth='')
+r = http.request('GET', url, headers=headers)
+r2 = BeautifulSoup(r, 'html.parser')
+print(r2)
+attr = {'class': ['job-title-link']}
+
+#print(soup)
+for link in soup.find_all('a', attr):
+    job = link.get('href')
+    #print(job)
+
+
