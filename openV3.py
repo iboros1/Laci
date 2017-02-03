0272c084e0ee4378ab88ce89a4203d48688446d7
Istvan Boros <istvan.boros@3pillarglobal.com>
2017-02-03 17:38:39 +0200

    Andrei

--- openV3.py
+++ openV3.py
@@ -8,13 +8,24 @@ import sqlite3
 import urllib3
 from time import strftime
 
-
 ### ALL WARNINGS ARE DISABLED
 urllib3.disable_warnings()
 
-http = httplib2.Http()
-web_bike_list = []
-unique_bike_list = []
+def get_web_bikes():
+    results = []
+    http = urllib3.PoolManager()
+    for current_page in range(NR_OF_PAGES_TO_OPEN):
+        response = http.request('GET', PAGE_TO_OPEN + '&page=' + str(current_page))
+        soup = BeautifulSoup(response.data, 'html.parser')
+        soup.prettify()
+        attr = {'class': [SEARCH_FOR_CLASS_ID]}
+        for beautiful_page_result in  soup.find_all('a', attr):
+            dirty_bike_link = beautiful_page_result.get('href')
+            bike_url, diez, unused_id = dirty_bike_link.partition('#')
+            title = beautiful_page_result.find_all('strong')
+            results.append((bike_url, title[0].text)) # create list with all the values
+    return results
+
 
 def get_db_bike_list():
     connect_to_db = sqlite3.connect(DB)
@@ -25,58 +36,27 @@ def get_db_bike_list():
     return [x[0] for x in db_list]
 
 
-def browse_pages(page):
-    http = urllib3.PoolManager()
-    response = http.request('GET', PAGE_TO_OPEN + '&page=' + str(page + 1))
-    soup = BeautifulSoup(response.data, 'html.parser')
-    soup.prettify()
-    attr = {'class': [SEARCH_FOR_CLASS_ID]}
-
-    for beautiful_page_result in  soup.find_all('a', attr):
-        dirty_bike_link = beautiful_page_result.get('href')
-        bike_url, diez, unused_id = dirty_bike_link.partition('#')
-        title = beautiful_page_result.find_all('strong')
-        web_bike_list.extend((bike_url, title))
-    return web_bike_list
-
-
-def write_to_db(get_db_bike_list,brows_pages):
-        if 'olx' in beautiful_page_result.attrs['href']:
-            bike_url, diez, unused_id = dirty_bike_link.partition('#')
-            title = beautiful_page_result.find_all('strong')
-            web_bike_list.append((bike_url, title))
-    return beautiful_page_result,web_bike_list
-
-
-def write_to_db(get_db_bike_list,web_bike_list):
-    db_add_date = strftime("%Y-%m-%d")
-    get_db_bike_list.db_cursor.execute(
-       "INSERT INTO Page (DateAdded, HtmlBike, AdName) VALUES ('%s','%s', '%s')" % (db_add_date, brows_pages.bike_url, brows_pages.title))
-
-
-def compare_lists(db_bike_list, web_bike_list):
-
+def compare_lists(db_bike_list, get_web_bikes):
     unique_bike_list = []
-    for bike in web_bike_list:
-        if bike not in db_bike_list:
-         unique_bike_list.append(bike)
-
-
-    for bike in web_bike_list[1]:
-        if bike[0] not in db_bike_list[0]:
+    for bike in get_web_bikes:
+        if bike[0] not in db_bike_list:
             unique_bike_list.append(bike)
         else:
             pass
+    return unique_bike_list
 
 
-def get_all_lists():
-    db_bike_list = get_db_bike_list()
-    for page in range(NR_OF_PAGES_TO_OPEN):
-        web_bike_list = browse_pages(page)
-        compare_lists(db_bike_list, web_bike_list)
-
-
+def write_to_db(compare_lists):
+    connect_to_db = sqlite3.connect(DB)
+    db_cusor = connect_to_db.cursor()
+    db_add_date = strftime("%Y-%m-%d")
+    db_cusor.execute(
+        "INSERT INTO Page (DateAdded, HtmlBike, AdName) VALUES ('%s','%s', '%s')" % (
+        db_add_date, compare_lists.bike_url, compare_lists.title))
 
 
 if __name__ == "__main__":
-    get_all_lists()
+    # get_all_lists()
+    db_bike_list = get_db_bike_list()
+    web_bike_list = get_web_bikes()
+    write_to_db(compare_lists(db_bike_list, web_bike_list))
