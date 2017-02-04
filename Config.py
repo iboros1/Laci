51e8683dcf9b80648a807b2a839adbb33dfb0f93
iboros1 <iborosppc@gmail.com>
2017-02-04 17:35:44 +0200

    home

--- Config.py
+++ Config.py
@@ -4,7 +4,7 @@ PAGE_TO_OPEN = "https://www.olx.ro/hobby-sport-turism/biciclete-fitness/biciclet
 SEARCH_FOR_CLASS_ID = "marginright5"
 
 
-NR_OF_PAGES_TO_OPEN = 3
+NR_OF_PAGES_TO_OPEN = 50
 
 
 DELETE_IF_OLDER_THEN = 1
