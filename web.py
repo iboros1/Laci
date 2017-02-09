15a9a825e7dd2b3056b44185ca64cbc27565ce34
iboros1 <iborosppc@gmail.com>
2017-02-09 22:08:17 +0200

    home

--- web.py
+++ web.py
@@ -2,6 +2,7 @@ import sqlite3
 from flask import Flask
 from config import DB
 import os.path
+from flask import jsonify, render_template
 
 
 
@@ -13,21 +14,36 @@ app = Flask(__name__)
 wsgi_app = app.wsgi_app
 
 
-@app.route('/')
 def open_db():
     #os.chdir("..")
     connect_to_db = sqlite3.connect(DB)
     db_cursor = connect_to_db.cursor()
-    db_cursor.execute('SELECT HtmlBike FROM Page')
+    db_cursor.execute('SELECT * FROM Page')
     db_list = db_cursor.fetchall()
-    do = []
+    bike = []
     for dit in db_list:
-        do.append(dit)
-    return do
-    connect_to_db.close()
-    return db_list
+        bike.append(dit)
+    date = [x[0] for x in bike]
+    html = [x[1] for x in bike]
+    title = [x[2] for x in bike]
+
+    return date, html, title
 
 
+@app.route('/')
+def test_json():
+    date, html, title = open_db()
+    return jsonify('results.html', DateAdded=date, HtmlBike=html, AdName=title)
+
+
+
+@app.route('/olx')
+def print_items():
+    #os.chdir("..")
+    connect_to_db = sqlite3.connect(DB)
+    db_cursor = connect_to_db.cursor()
+    db_cursor.execute('SELECT * FROM Page')
+    return render_template('results.html', items=db_cursor.fetchall())
 
 
 
